113b647de237cc475e7ab544f667416c8b00a0c8
shrushtir2006 <shrushtiryavanki@gmail.com>
2025-12-01 17:10:08 +0530

    Update product.py

--- product.py
+++ product.py
@@ -1,17 +1,14 @@
-def product_details(name, p_id, quantity, price):
-    result = (
-        f"Product Name: {name}\n"
-        f"Product ID: {p_id}\n"
+def product_info(product_id, name, quantity, price):
+    return (
+        f"Product ID: {product_id}\n"
+        f"Name: {name}\n"
         f"Quantity: {quantity}\n"
         f"Price: {price}"
     )
-    return result
-
 if __name__ == "__main__":
-   
-        name = "Shampoo\n"
-        p_id = "E1001\n"
-        quantity = "3\n"
-        price = 1000
-
-        print(product_details(name, p_id, quantity, price))
+    product_id = "P102"
+    name = "Keyboard"
+    quantity = 5
+    price = 799.50
+    print("Product Details:\n")
+    print(product_info(product_id, name, quantity, price))
