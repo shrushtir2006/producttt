88a45a6331b27c702eabbfd84afb3daceed01db1
shrushtir2006 <shrushtiryavanki@gmail.com>
2025-12-03 22:20:21 +0530

    Update product.py

--- product.py
+++ product.py
@@ -1,14 +1,14 @@
-def product_details(product_id, name, quantity, price):
+def product_info(product_id, name, quantity, price):
     return (
         f"Product ID: {product_id}\n"
-        f"ProductName: {name}\n"
+        f"Name: {name}\n"
         f"Quantity: {quantity}\n"
         f"Price: {price}"
     )
 if __name__ == "__main__":
-    pid = "P101"
-    pname = "Laptop"
-    qty = 5
-    pr = 50000
-    
-    print(Product_Details(pid,pname,qty,pr))
+    product_id = "P102"
+    name = "Keyboard"
+    quantity = 5
+    price = 799.50
+    print("Product Details:\n")
+    print(product_info(product_id, name, quantity, price))
